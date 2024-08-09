2361cf02d17b9243ce9f9ebc04ce934da7a36423
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-09 20:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let container =  document.querySelector(".container")
+
+for(let i=0; i<800; i++){
+ let square = document.createElement("div")
+square.className = "square";
+square.style.backgroundColor = "grey"
+container.append(square)
+}
