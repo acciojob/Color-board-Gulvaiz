eaf965afb1a07c9fc6c8ca58a93bc67ac3095c99
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-12 00:19:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let container = document.querySelector(".container");
+let container = document.querySelector('.container')
 
-for (let i = 0; i < 800; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+for(let i=0; i<800; i++){
+    let square = document.createElement('div')
+    square.classList.add('square');
+    square.style.backgroundColor = 'rgb(128, 128, 128)'
+    container.appendChild(square)
 }
 
 const squares = document.querySelectorAll('.square');
-
 // Add event listeners to each square
 squares.forEach(square => {
     square.addEventListener('mouseover', () => {
